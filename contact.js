f2d43f64089ac98a67bfd23a20236115b0ff795b
SBchandrasekara <sbchandrasekara404@gmail.com>
2024-12-13 20:17:56 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -5,3 +5,11 @@ inputs.forEach(input => {
     input.setAttribute('placeholder', '');
   });
 
+  input.addEventListener('blur', () => {
+    if (input.id === 'first-name') input.setAttribute('placeholder', 'First name');
+    if (input.id === 'last-name') input.setAttribute('placeholder', 'Last name');
+    if (input.id === 'email') input.setAttribute('placeholder', 'Your Email');
+    if (input.id === 'phone') input.setAttribute('placeholder', 'Phone Number');
+    if (input.id === 'message') input.setAttribute('placeholder', 'How can we help?');
+  });
+});
